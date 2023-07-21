9824470ccbb877411db8fd36fb631e25af8ad8cf
Muhammad Raihan Suryanom <muhammad.suryanom@btpn.com>
2023-07-21 23:08:12 +0700

    chore: setup LPMQ and poppins fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        lpmq: ['var(--font-lpmq)'],
+        poppins: ['var(--font-poppins)'],
+      },
     },
   },
   plugins: [],
