777e576be397945d4991e963ddaacccaf28b7efa
Muhammad Raihan Suryanom <muhammad.suryanom@btpn.com>
2023-10-06 01:12:02 +0700

    feat:[WIP] add dummy static params in  route

--- src/app/books/[id]/[page]/page.tsx
+++ src/app/books/[id]/[page]/page.tsx
@@ -1,1 +1,17 @@
-export default () => null; // eslint-disable-line import/no-anonymous-default-export
+export async function generateStaticParams() {
+  return [
+    { id: 'dummy-template', page: '1' },
+    { id: 'dummy-template', page: '2' },
+    { id: 'dummy-template', page: '3' },
+    { id: 'dummy-template', page: '4' },
+    { id: 'dummy-template', page: '5' },
+    { id: 'dummy-template', page: '6' },
+    { id: 'dummy-template', page: '7' },
+    { id: 'dummy-template', page: '8' },
+    { id: 'dummy-template', page: '9' },
+    { id: 'dummy-template', page: '10' },
+  ];
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => null;
