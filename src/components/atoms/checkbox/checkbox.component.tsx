e87ce48ede0c7080e84ccf4ca8ec11240bcfca0b
Muhammad Raihan Suryanom <muhammad.suryanom@btpn.com>
2023-10-19 23:19:54 +0700

    fix: rewrite radix primitives imports to reduce bundle size

--- src/components/atoms/checkbox/checkbox.component.tsx
+++ src/components/atoms/checkbox/checkbox.component.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import { Check } from 'lucide-react';
 import { forwardRef } from 'react';
+import { Root, Indicator } from '@radix-ui/react-checkbox';
+import { Check } from 'lucide-react';
 
 import { cn } from '~/lib/utils';
 
 const Checkbox = forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+  React.ElementRef<typeof Root>,
+  React.ComponentPropsWithoutRef<typeof Root>
 >(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
+  <Root
     ref={ref}
     className={cn(
       'peer h-6 w-6 shrink-0 rounded-md border-2 border-[#c3c3c3] ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-light focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary-light data-[state=checked]:bg-primary-light data-[state=checked]:text-slate-50 dark:border-slate-50 dark:ring-offset-primary-light dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900',
@@ -18,12 +18,12 @@ const Checkbox = forwardRef<
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
+    <Indicator className="flex items-center justify-center text-current">
       <Check className="h-4 w-4" strokeWidth={4.5} />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
+    </Indicator>
+  </Root>
 ));
 
-Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+Checkbox.displayName = Root.displayName;
 
 export { Checkbox };
