d20e8c742e1e9f7e10e37a89fd355ccf839e9215
Muhammad Raihan Suryanom <muhammad.suryanom@btpn.com>
2023-11-17 15:00:48 +0700

    feat: enabled appropiate fetch reponse with success and error state

--- src/lib/utils/wait.ts
+++ src/lib/utils/wait.ts
@@ -1,3 +1,5 @@
+import { FetchResponse } from './index.type';
+
 export const wait = (milliSecond: number) =>
   new Promise((resolve) => setTimeout(resolve, milliSecond));
 
@@ -5,13 +7,23 @@ export const Await = async <T>({
   promise,
   children,
   delay = 0,
+  _DEV,
 }: {
   promise: Promise<T>;
   children: (value: T) => JSX.Element;
   delay?: number;
+  _DEV?: boolean;
 }) => {
   await wait(delay); // WARNING: DON'T USE THIS, ONLY FOR DEBUGGING PURPOSE!
-  const data = await promise;
+  const response = (await promise) as FetchResponse<T>;
 
-  return children(data);
+  if (_DEV) {
+    return children(response as T);
+  }
+
+  if (response.statusCode === 200) {
+    return children(response.data);
+  }
+
+  throw new Error(response.message);
 };
