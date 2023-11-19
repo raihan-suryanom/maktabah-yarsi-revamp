5b1b6d28029b845a7f8691aa40ea0f8da2fddbf2
Muhammad Raihan Suryanom <muhammad.suryanom@btpn.com>
2023-11-19 10:33:18 +0700

    fix:[WIP] fetching dummy categories on prod env

--- src/lib/utils/categories.server.ts
+++ src/lib/utils/categories.server.ts
@@ -1,6 +1,14 @@
 import { CategoryProps } from '~/components/molecules/collapsible-menu';
+import dummyCategories from '../fixtures/categories';
 
-export const getCategories = async (): Promise<ReadonlyArray<CategoryProps>> =>
-  fetch('http://localhost:5555/api/v1/category').then((response) =>
+export const getCategories = async (): Promise<
+  ReadonlyArray<CategoryProps>
+> => {
+  if (process.env.NODE_ENV === 'production') {
+    return dummyCategories as Promise<ReadonlyArray<CategoryProps>>;
+  }
+
+  return fetch('http://localhost:5555/api/v1/category').then((response) =>
     response.json()
   );
+};
