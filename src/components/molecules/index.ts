318bc17931725f0c7d76ea62cc30cb1d0864f7ee
Muhammad Raihan Suryanom <muhammad.suryanom@btpn.com>
2023-12-25 21:07:13 +0700

    fix: remove radix accordion completely

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,7 +2,6 @@ import { Breadcrumb } from './breadcrumb';
 import { NavigationBar } from './navigation-bar';
 import { Pagination } from './pagination';
 import { SearchButton } from './search-button';
-import { CollapsibleMenu } from './collapsible-menu';
 import { SearchForm } from './search-form';
 import { CardGroup } from './card-group';
 import PageControlComponent from './page-control/page-control.component';
@@ -13,7 +12,6 @@ export {
   Pagination,
   SearchButton,
   SearchForm,
-  CollapsibleMenu,
   CardGroup,
   PageControlComponent,
 };
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,14 +1,6 @@
 import { BookList } from './book-list';
 import { MainContent } from './main-content';
-import { MenuOutline } from './menu-outline';
 import { SearchTable } from './search-table';
 import { ThemeProvider, ThemeToggle } from './theme';
 
-export {
-  BookList,
-  MainContent,
-  MenuOutline,
-  SearchTable,
-  ThemeProvider,
-  ThemeToggle,
-};
+export { BookList, MainContent, SearchTable, ThemeProvider, ThemeToggle };
